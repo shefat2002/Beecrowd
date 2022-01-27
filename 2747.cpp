9ba7fa941060f09d5b7e4e4df715c4d81a759438
shefat2002 <67193133+shefat2002@users.noreply.github.com>
2022-01-28 00:46:54 +0600

    Add files via upload

---
+++ 2747.cpp
@@ -0,0 +1,16 @@
+//Make sure you have tried more than three times before watching this code!!
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "---------------------------------------" << endl;
+    cout << "|                                     |" << endl;
+    cout << "|                                     |" << endl;
+    cout << "|                                     |" << endl;
+    cout << "|                                     |" << endl;
+    cout << "|                                     |" << endl;
+    cout << "---------------------------------------" << endl;
+    return 0;
+}
---
+++ 2748.cpp
@@ -0,0 +1,17 @@
+//Make sure you have tried more than three times before watching this code!!
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "---------------------------------------" << endl;
+    cout << "|        Roberto                      |" << endl;
+    cout << "|                                     |" << endl;
+    cout << "|        5786                         |" << endl;
+    cout << "|                                     |" << endl;
+    cout << "|        UNIFEI                       |" << endl;
+    cout << "---------------------------------------" << endl;
+    return 0;
+}
+
