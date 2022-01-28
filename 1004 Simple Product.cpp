3d36e3083c0e7155c0abbc38a8225a815dd04374
shefat2002 <67193133+shefat2002@users.noreply.github.com>
2022-01-28 10:14:41 +0600

    Create 1004 Simple Product.cpp

---
+++ 1004 Simple Product.cpp
@@ -0,0 +1,14 @@
+//Don't copy my code. Try it yourself!!! @Shefat
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a , b;
+    cin >> a >> b;
+    cout << "PROD = " << a * b <<endl;
+
+    return 0;
+}
+
