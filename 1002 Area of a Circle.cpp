01b2a2c1c65679cc318af8689348fd01ca2d0f6c
shefat2002 <67193133+shefat2002@users.noreply.github.com>
2022-01-28 10:36:07 +0600

    Create 1002 Area of a Circle.cpp

---
+++ 1002 Area of a Circle.cpp
@@ -0,0 +1,13 @@
+//Make sure you have tried more than three times before watching this code!!
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double pi = 3.14159 , r;
+    cin >> r;
+    printf("A=%.4lf\n" , pi*r*r);
+
+    return 0;
+}
