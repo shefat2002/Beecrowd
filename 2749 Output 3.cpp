1b22dd13be78e4d1e6a3cadc5a910a9542f25688
shefat2002 <67193133+shefat2002@users.noreply.github.com>
2022-01-28 23:46:01 +0600

    Add files via upload

---
+++ 2749 Output 3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "---------------------------------------" << endl;
+    cout << "|x = 35                               |" << endl;
+    cout << "|                                     |" << endl;
+    cout << "|                x = 35               |" << endl;
+    cout << "|                                     |" << endl;
+    cout << "|                               x = 35|" << endl;
+    cout << "---------------------------------------" << endl;
+
+    return 0;
+}
