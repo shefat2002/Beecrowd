08a1e91ffe10aa6577df643ea611e4f6bc52114e
shefat2002 <67193133+shefat2002@users.noreply.github.com>
2022-01-29 11:35:22 +0600

    Create  2762 - Input and Output 6.cpp

---
+++ 2762 - Input and Output 6.cpp
@@ -0,0 +1,12 @@
+//do not copy and submit this code.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x , y;
+    scanf("%d.%d" , &x , &y);
+    cout << y << "." << x << endl;
+    return 0;
+}
+
