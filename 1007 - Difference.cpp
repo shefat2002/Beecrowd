eb68f882646d5a9ab5585d0f3d6e1b6a90e90719
shefat2002 <67193133+shefat2002@users.noreply.github.com>
2022-01-29 12:24:15 +0600

    Create 1007 - Difference.cpp

---
+++ 1007 - Difference.cpp
@@ -0,0 +1,15 @@
+//Please Try yourself!!
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a , b , c , d;
+
+    cin >>a >> b >> c >> d;
+
+    cout << "DIFERENCA = " << (a*b) - (c*d) << endl;
+
+    return 0;
+}
