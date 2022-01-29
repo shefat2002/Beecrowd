3f5aaf083980d643918edc4df98f9ea98b940c7a
shefat2002 <67193133+shefat2002@users.noreply.github.com>
2022-01-29 12:19:52 +0600

    Create 2754 - Output 8.cpp

---
+++ 2754 - Output 8.cpp
@@ -0,0 +1,28 @@
+//do not copy and submit the code. Try yourself
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    double c , d;
+
+    c =  234.345;
+    d = 45.698;
+
+
+
+    printf("%.6lf - %.6lf\n", c , d);
+    printf("%.0lf - %.0lf\n" , c , d);
+    printf("%.1lf - %.1lf\n" , c , d);
+    printf("%.2lf - %.2lf\n" , c , d);
+    printf("%.3lf - %.3lf\n" , c , d);
+    printf("%e - %e\n" ,c , d);
+    printf("%E - %E\n" ,c , d);
+    printf("%.3lf - %.3lf\n" , c , d);
+    printf("%g - %g\n" , c , d);
+
+
+    return 0;
+}
