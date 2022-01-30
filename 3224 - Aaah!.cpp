c9a49fa9eec95edc8b2eb872ca05ce8c88b8af3d
shefat2002 <67193133+shefat2002@users.noreply.github.com>
2022-01-30 12:52:32 +0600

    Create  3224 - Aaah!.cpp

---
+++ 3224 - Aaah!.cpp
@@ -0,0 +1,17 @@
+// Make sure you've tried enough times before watch this code!!
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string a , b;
+    cin >>a >> b;
+    if(a.length() < b.length()){
+        cout << "no" << endl;
+    }
+    else
+        cout << "go" << endl;
+
+    return 0;
+}
