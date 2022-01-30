3ab12e1195a69d63e6ef9510f33724ed8bbf75eb
shefat2002 <67193133+shefat2002@users.noreply.github.com>
2022-01-30 11:37:08 +0600

    Create  3302 - Correct Answer.cpp

---
+++ 3302 - Correct Answer.cpp
@@ -0,0 +1,14 @@
+//Do not copy. Try yourself!!!
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n , a;
+    cin >> n;
+    for(int i = 1 ; i <=n ; i++){
+        cin >> a;
+        cout << "resposta " << i << ": " << a << endl;
+    }
+    return 0;
+}
