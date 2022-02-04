2e930c71ee0bd01a47d8df0cf7645f82ef6bf125
shefat2002 <67193133+shefat2002@users.noreply.github.com>
2022-02-04 22:52:30 +0600

    Create 2755 - Output 9.cpp

---
+++ 2755 - Output 9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("\"Ro'b'er\tto\\/\"\n");
+    printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
+    printf("(^_-) (-_-) (-_^)\n");
+    printf("(\"_\") ('.')\n");
+
+    return 0;
+}
