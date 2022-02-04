8c0941f29d0313727c2af08164e82dfcea1e5dc9
shefat2002 <67193133+shefat2002@users.noreply.github.com>
2022-02-04 23:26:44 +0600

    Create 2753 - Output 7.cpp

---
+++ 2753 - Output 7.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 97, b, c ,d, e ,f ,g ,h ,i , j , k , l , m , n , o , p , q,  r , s, t , u , v , w , x , y , z;
+    b = a+1;
+    c = b+1;
+    d = c+1;
+    e = d+1;
+    f = e+1;
+    g = f+1;
+    h = g+1;
+    i = h+1;
+    j = i+1;
+    k = j+1;
+    l = k+1;
+    m = l+1;
+    n = m+1;
+    o = n+1;
+    p = o+1;
+    q = p+1;
+    r = q+1;
+    s = r+1;
+    t = s+1;
+    u = t+1;
+    v = u+1;
+    w = v+1;
+    x = w+1;
+    y = x+1;
+    z = y+1;
+
+    printf("%d e %c\n" , a , a);
+    printf("%d e %c\n" , b , b);
+    printf("%d e %c\n" , c , c);
+    printf("%d e %c\n" , d , d);
+    printf("%d e %c\n" , e , e);
+    printf("%d e %c\n" , f , f);
+    printf("%d e %c\n" , g , g);
+    printf("%d e %c\n" , h , h);
+    printf("%d e %c\n" , i , i);
+    printf("%d e %c\n" , j , j);
+    printf("%d e %c\n" , k , k);
+    printf("%d e %c\n" , l , l);
+    printf("%d e %c\n" , m , m);
+    printf("%d e %c\n" , n , n);
+    printf("%d e %c\n" , o , o);
+    printf("%d e %c\n" , p , p);
+    printf("%d e %c\n" , q , q);
+    printf("%d e %c\n" , r , r);
+    printf("%d e %c\n" , s , s);
+    printf("%d e %c\n" , t , t);
+    printf("%d e %c\n" , u , u);
+    printf("%d e %c\n" , v , v);
+    printf("%d e %c\n" , w , w);
+    printf("%d e %c\n" , x , x);
+    printf("%d e %c\n" , y , y);
+    printf("%d e %c\n" , z , z);
+
+
+
+    return 0;
+}
