b78ba8f32412a46c9faff77356cb102718f90728
Shefat Al Mahmud <shefat2002@gmail.com>
2022-02-08 01:05:01 +0600

    Create 2750 - Output 4.cpp

---
+++ 2750 - Output 4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "---------------------------------------" << endl;
+    cout << "|  decimal  |  octal  |  Hexadecimal  |" << endl;
+    cout << "---------------------------------------" << endl;
+    cout << "|      0    |    0    |       0       |" << endl;
+    cout << "|      1    |    1    |       1       |" << endl;
+    cout << "|      2    |    2    |       2       |" << endl;
+    cout << "|      3    |    3    |       3       |" << endl;
+    cout << "|      4    |    4    |       4       |" << endl;
+    cout << "|      5    |    5    |       5       |" << endl;
+    cout << "|      6    |    6    |       6       |" << endl;
+    cout << "|      7    |    7    |       7       |" << endl;
+    cout << "|      8    |   10    |       8       |" << endl;
+    cout << "|      9    |   11    |       9       |" << endl;
+    cout << "|     10    |   12    |       A       |" << endl;
+    cout << "|     11    |   13    |       B       |" << endl;
+    cout << "|     12    |   14    |       C       |" << endl;
+    cout << "|     13    |   15    |       D       |" << endl;
+    cout << "|     14    |   16    |       E       |" << endl;
+    cout << "|     15    |   17    |       F       |" << endl;
+    cout << "---------------------------------------" << endl;
+    return 0;
+}
