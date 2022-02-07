50f65ca1bc30780e34b4bfd0803fdbee34c73b53
Shefat Al Mahmud <shefat2002@gmail.com>
2022-02-07 11:54:16 +0600

    Create  2861 - The Output.cpp

---
+++ 2861 - The Output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s , a = "gzuz";
+    int n;
+    cin >> n;
+    for(int i = 0 ; i < n ; i++){
+        scanf("\n");
+        getline(cin , s);
+
+        cout << a << endl;
+    }
+
+    return 0;
+}
