935e61470a84413134c8bbd9755a3bc698484afb
Shefat Al Mahmud <shefat2002@gmail.com>
2022-02-07 11:38:35 +0600

    Create 1864 - Our Days Are Never Coming Back.cpp

---
+++ 1864 - Our Days Are Never Coming Back.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    int n;
+    cin >> n;
+    for(int i = 0 ; i < n ; i++){
+        cout << s[i];
+    }
+    cout << endl;
+
+    return 0;
+}
+
