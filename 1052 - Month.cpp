ee6390843c67b2d0e82bf8eb7c48fcf403257380
Shefat Al Mahmud <shefat2002@gmail.com>
2022-02-08 12:15:24 +0600

    Create 1052 - Month.cpp

---
+++ 1052 - Month.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    int i;
+    
+    scanf("%d" , &i);
+    
+    if(i == 1) printf("January\n");
+    else if(i == 2) printf("February\n");
+    else if(i == 3) printf("March\n");
+    else if(i == 4) printf("April\n");
+    else if(i == 5) printf("May\n");
+     else if(i == 6) printf("June\n");
+     else if(i == 7) printf("July\n");
+     else if(i == 8) printf("August\n");
+     else if(i == 9) printf("September\n");
+     else if(i == 10) printf("October\n");
+     else if(i == 11) printf("November\n");
+     else if(i == 12) printf("December\n");
+ 
+ 
+    return 0;
+}
