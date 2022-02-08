1d29a605ba42e213f3770342226ec74dee207c76
Shefat Al Mahmud <shefat2002@gmail.com>
2022-02-08 12:04:26 +0600

    Create  1005 - Average 1.cpp

---
+++ 1005 - Average 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float a , b , m;
+    scanf("%f%f" , &a , &b);
+
+    m = ((3.5*a)+(7.5*b))/(3.5+7.5);
+
+    printf("MEDIA = %.5f\n" , m);
+
+    return 0;
+}
