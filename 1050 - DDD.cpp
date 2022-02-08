5a9cda1b3a73596d77dae66426b1ee6e4965920f
Shefat Al Mahmud <shefat2002@gmail.com>
2022-02-08 12:14:41 +0600

    Create 1050 - DDD.cpp

---
+++ 1050 - DDD.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    int i;
+    scanf("%d" , &i);
+    
+    if(i == 61 ){
+        printf("Brasilia\n");
+    }
+    else if(i == 71 ){
+        printf("Salvador\n");
+    }
+    else if(i == 11 ){
+        printf("Sao Paulo\n");
+    }
+    else if(i == 21 ){
+        printf("Rio de Janeiro\n");
+    }
+    else if(i == 32 ){
+        printf("Juiz de Fora\n");
+    }
+    else if(i == 19 ){
+        printf("Campinas\n");
+    }
+    else if(i == 27 ){
+        printf("Vitoria\n");
+    }
+    else if(i == 31 ){
+        printf("Belo Horizonte\n");
+    }
+    else{
+        printf("DDD nao cadastrado\n");
+    }
+    
+ 
+    return 0;
+}
