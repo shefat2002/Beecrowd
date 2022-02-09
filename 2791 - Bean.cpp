7d1d7c2b849149032f2e4c978dc4d04a235c0900
Shefat Al Mahmud <shefat2002@gmail.com>
2022-02-10 01:54:57 +0600

    Create 2791 - Bean.cpp

---
+++ 2791 - Bean.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a ;
+    for(int i = 1 ; i <= 4; i++){
+        cin >> a;
+        if(a == 1){
+            cout << i << endl;
+            break;
+        }
+
+    }
+
+
+    return 0;
+}
+
