d90dfae26aedd7e5aeefb444af035bdeeb76e722
Shefat Al Mahmud <shefat2002@gmail.com>
2022-02-10 01:49:18 +0600

    Create 2717 - Elf Time.cpp

---
+++ 2717 - Elf Time.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int a , b;
+    cin >> a >> b;
+    if(n >= a+b) cout << "Farei hoje!\n";
+    else cout << "Deixa para amanha!\n";
+    return 0;
+}
+
