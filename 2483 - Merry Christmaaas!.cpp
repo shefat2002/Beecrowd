94164e8b1243ce84ccc24bfa1690aa513aead6f9
Shefat Al Mahmud <shefat2002@gmail.com>
2022-02-10 01:33:03 +0600

    Create 2483 - Merry Christmaaas!.cpp

---
+++ 2483 - Merry Christmaaas!.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c = 'a';
+    int n;
+    cin >> n;
+    cout << "Feliz nat";
+    for(int i = 0; i < n ; i++){
+        cout << c;
+    }
+    cout << "l!" << endl;
+
+    return 0;
+}
