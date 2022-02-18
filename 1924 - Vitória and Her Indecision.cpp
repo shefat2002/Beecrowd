6e984e72c1fa433bd3f60dcf6825821d34f1a85d
Shefat Al Mahmud <shefat2002@gmail.com>
2022-02-18 16:29:00 +0600

    Create 1924 - Vitória and Her Indecision.cpp

---
+++ 1924 - Vitória and Her Indecision.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n ;
+    string s;
+    cin >> n;
+
+    for(int i = 0; i < n ; i++){
+        cin >> s;
+        getline(cin , s);
+
+    }
+    cout << "Ciencia da Computacao" << endl;
+    return 0;
+}
+
