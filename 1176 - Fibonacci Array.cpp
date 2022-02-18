e28117332c89cac52c4ced48fee798c439bbdf4a
Shefat Al Mahmud <shefat2002@gmail.com>
2022-02-18 12:42:15 +0600

    Update 1176 - Fibonacci Array.cpp

--- 1176 - Fibonacci Array.cpp
+++ 1176 - Fibonacci Array.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int f(int a)
-{
-    if(a == 0) return 0;
-    if(a == 1 || a ==2) return 1;
-    return f(a-1) +f(a-2);
-}
 
 int main()
 {
     int n , a;
+    long long int f[61];
+    f[0] = 0;
+    f[1] = 1;
     cin >> n;
-    for(int i=0; i < n; i++){
+    for(int i=2; i < 61; i++){
+        f[i] = f[i-1] + f[i-2];
+    }
+    for(int i = 0 ; i < n ; i++){
         cin >> a;
-        printf("Fib(%d) = %d\n" , a , f(a) );
-
+        printf("Fib(%d) = %lld\n" , a , f[a] );
     }
 
     return 0;
