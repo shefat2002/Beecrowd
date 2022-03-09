f5d8cccb4237d95d7a85df6d6a2adb1b5b590d09
Shefat Al Mahmud <shefat2002@gmail.com>
2022-03-09 15:54:29 +0600

    Create 1805 - Natural Sum.cpp

---
+++ 1805 - Natural Sum.cpp
@@ -0,0 +1,15 @@
+///1805 - Natural Sum
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    unsigned long long int a ,b ;
+    cin >> a >> b;
+
+
+
+    cout << ((b+a)*(b-a+1))/2 << endl;
+
+    return 0;
+}
