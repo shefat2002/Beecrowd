e44ba6d636e979c02692dc06b2827187f060d5d8
Shefat Al Mahmud <shefat2002@gmail.com>
2022-03-13 19:22:21 +0600

    Create  2160 - Name at Form.cpp

---
+++ 2160 - Name at Form.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin , s);
+    int l = s.length();
+    if(l <= 80) cout << "YES\n";
+    else cout << "NO\n";
+
+    return 0;
+}
+
