47e95007e7b74266a8fcdccab875d7787cd76487
Shefat Al Mahmud <shefat2002@gmail.com>
2022-03-14 21:22:53 +0600

    Create  1184 - Below the Main Diagonal.cpp

---
+++ 1184 - Below the Main Diagonal.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a[12][12];
+    char c;
+    cin >> c;
+    double sum = 0;
+    for(int i = 0 ; i < 12; i++){
+        for(int j = 0 ; j < 12; j++){
+            cin >> a[i][j];
+        }
+    }
+    int u = 0;
+    for(int i = 0 ; i < 12; i++){
+        for(int j = 0 ; j < u; j++){
+            sum += a[i][j];
+        }
+        u++;
+
+    }
+    if(c == 'S') printf("%.1f\n" , sum);
+    else if(c == 'M') printf("%.1f\n" , sum/66);
+
+
+    return 0;
+}
+
+
+
