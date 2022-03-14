d51f6de57edf8ad2596ba17d93c076a8a117e90b
Shefat Al Mahmud <shefat2002@gmail.com>
2022-03-14 19:22:56 +0600

    Create  1183 - Above the Main Diagonal.cpp

---
+++ 1183 - Above the Main Diagonal.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a[12][12];
+    char c;
+    cin >> c;
+    double sum = 0;
+    for(int i = 0 ; i < 12; i++){
+        for(int j = 0 ; j < 12; j++){
+            cin >> a[i][j];
+        }
+    }
+    int u = 1;
+    for(int i = 0 ; i < 12; i++){
+        for(int j = u ; j < 12; j++){
+            sum += a[i][j];
+        }
+        u++;
+
+    }
+    if(c == 'S') printf("%.1f\n" , sum);
+    else if(c == 'M') printf("%.1f\n" , sum/66);
+
+
+    return 0;
+}
+
