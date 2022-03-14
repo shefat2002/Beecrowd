d059623df2ab3db30cbd4c3d71bddbc6486d0331
Shefat Al Mahmud <shefat2002@gmail.com>
2022-03-14 13:59:26 +0600

    Create 1181 - Line in Array.cpp

---
+++ 1181 - Line in Array.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a[12][12];
+    int n;
+    cin >> n;
+    char c;
+    cin >> c;
+    double sum = 0;
+    for(int i = 0 ; i < 12; i++){
+        for(int j = 0 ; j < 12; j++){
+            cin >> a[i][j];
+            if(i == n) sum += a[i][j];
+        }
+    }
+    if(c == 'S') printf("%.1f\n" , sum);
+    else if(c == 'M') printf("%.1f\n" , sum/12);
+
+
+    return 0;
+}
