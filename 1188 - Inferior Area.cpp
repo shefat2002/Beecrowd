e1fdef43edcce2d0f49f865b91a73e3eee3f4995
Shefat Al Mahmud <shefat2002@gmail.com>
2022-03-15 01:22:42 +0600

    Create  1188 - Inferior Area.cpp

---
+++ 1188 - Inferior Area.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char c;
+    cin >> c;
+    double sum = 0;
+    double a[12][12];
+    for(int i = 0 ; i < 12; i++){
+        for(int j = 0 ; j < 12 ; j++){
+            cin >> a[i][j];
+            if(j > (11-i) && j< i) sum += a[i][j];
+        }
+    }
+
+
+    if(c == 'S') printf("%.1lf\n" , sum);
+    else if(c == 'M') printf("%.1lf\n" , sum/30);
+    return 0;
+}
