fb9c341f4f706f9797831387eb75825e07bffdcb
Shefat Al Mahmud <shefat2002@gmail.com>
2022-03-17 00:00:45 +0600

    Update 2176 - Parity.cpp

--- 2176 - Parity.cpp
+++ 2176 - Parity.cpp
@@ -6,9 +6,7 @@ int main()
     cin >> s;
     int c = 0;
     for(int i = 0 ; i < s.length() ; i++){
-        if(s[i] == '1'){
-            c++;
-        }
+        if(s[i] == '1') c++;
     }
     if(c % 2 == 0) cout << s << 0 << endl;
     else cout << s << 1 << endl;
