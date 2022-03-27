400168988c8eb192f7d64e0368fce4426c41458b
Shefat Al Mahmud <shefat2002@gmail.com>
2022-03-27 13:18:49 +0600

    Update 2534 - General Exam.cpp

--- 2534 - General Exam.cpp
+++ 2534 - General Exam.cpp
@@ -1,28 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define ar 30050
 int main()
 {
     int n  ,p =0;
-    cin >> n  >> p;
-    int N[n] , P[p], R[p];
-    for(int i = 0 ; i < n ; i++){
-        cin >> N[i];
-    }
-    for(int i = 0; i< p ; i++){
-        cin >> P[i];
-    }
-    int s = sizeof(N) / sizeof(N[0]);
-    sort(N , N+ s);
-    for(int i = s, j = 0; i >= 0 ; i-- , j++){
+    while(cin >> n  >> p){
+        int N[n] , P[ar], R[ar];
+        for(int i = 0 ; i < n ; i++){
+            cin >> N[i];
+        }
+        for(int i = 0; i< p ; i++){
+            cin >> P[i];
+        }
+        int s = sizeof(N) / sizeof(N[0]);
+        sort(N , N+ s);
+        for(int i = s, j = 0; i >= 0 ; i-- , j++){
             R[j] = N[i];
+        }
+        for(int i = 0 ; i < p; i++){
+            int t = P[i];
+            cout << R[t] << "\n";
+        }
     }
-    for(int i = 0 ; i < p; i++){
-        int t = P[i];
-        cout << R[t] << "\n";
-    }
-
-
     return 0;
 }
 
