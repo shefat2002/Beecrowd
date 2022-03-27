4ccdf9b5845a3b670650ab86f44a423b37fc347d
Shefat Al Mahmud <shefat2002@gmail.com>
2022-03-28 00:42:59 +0600

    Update 2355 - Brazil and Germany.cpp

--- 2355 - Brazil and Germany.cpp
+++ 2355 - Brazil and Germany.cpp
@@ -5,12 +5,11 @@ int main()
     int n;
     while(cin >> n){
         if(n == 0) break;
-        double b = ceil(n/90);
-        double g = ceil((7*n)/90);
+        double b = round(n/90);
+        double g = round((7*n)/90);
         cout << "Brasil " << b << " x Alemanha "<< g<< "\n";
     }
 
 
     return 0;
 }
-
