1b7b3d5655886643c89832430b6eef2d842f587e
Shefat Al Mahmud <shefat2002@gmail.com>
2022-03-27 15:21:16 +0600

    Create 1190 - Right Area.cpp

---
+++ 1190 - Right Area.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char c;
+    cin >> c;
+    double m[12][12];
+    for(int i = 0 ; i < 12; i++){
+        for(int j = 0 ; j < 12; j++){
+            cin >> m[i][j];
+        }
+    }
+    double inc1 =11, inc2 =7 , sum =0;
+    for(int i = 1 ; i <= 10; i++){
+        if(i<=5){
+            for(int j = inc1 ; j < 12 ; j++){
+                sum+=m[i][j];
+            }
+            inc1--;
+        }
+        else if(i>=6){
+            for(int j = inc2 ; j < 12 ; j++){
+                sum+=m[i][j];
+            }
+            inc2++;
+        }
+    }
+    if(c == 'S') cout << fixed << setprecision(1) << sum << endl;
+    else if(c== 'M') if(c == 'S') cout << fixed << setprecision(1) << sum/30 << endl;
+    return 0;
+}
+
