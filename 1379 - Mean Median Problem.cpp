4b5ac04906c7d2822b831b3ea1a662f67ecc8ac5
Shefat Al Mahmud <shefat2002@gmail.com>
2022-03-28 11:59:45 +0600

    Create 1379 - Mean Median Problem.cpp

---
+++ 1379 - Mean Median Problem.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a , b;
+    while(cin >> a >> b && a !=0 && b!=0){
+        if(a>b) swap(a ,b);
+        cout << a - (b-a) << "\n";
+    }
+
+    return 0;
+}
