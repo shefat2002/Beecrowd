dcd73579b50df4cdab4edc86ed6c797ded61ef01
Shefat Al Mahmud <shefat2002@gmail.com>
2022-03-31 16:20:13 +0600

    Create 1300 - Hours and Minutes.cpp

---
+++ 1300 - Hours and Minutes.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin >> n){
+        if(n %6 == 0) cout << "Y\n";
+        else cout << "N\n";
+    }
+
+    return 0;
+}
+
