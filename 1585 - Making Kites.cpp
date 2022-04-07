fa9bf01e540638cf237d9a6e89b7438b42924adb
Shefat Al Mahmud <shefat2002@gmail.com>
2022-04-07 10:23:21 +0600

    Create 1585 - Making Kites.cpp

---
+++ 1585 - Making Kites.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while(n--){
+        int a , b;
+        cin >> a>> b;
+        cout << (a*b)/2 << " cm2" << endl;
+    }
+
+    return 0;
+}
