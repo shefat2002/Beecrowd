2750b1ef0d2f622912c9d56d627ad4a20bb0d700
Shefat Al Mahmud <shefat2002@gmail.com>
2022-07-13 19:54:25 +0600

    Update 1318 - Fake Tickets.cpp

--- 1318 - Fake Tickets.cpp
+++ 1318 - Fake Tickets.cpp
@@ -1,28 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
 
 int main()
 {
-    int m , n ,a ;
-
-    while(cin >> m >> n){
-        if(m == 0 && n ==0) break;
-        int t[100000] , count = 0;
-        for(int i = 1; i <= m ; i++){
-            t[i] = 0;
-        }
-        for(int i = 0 ; i < n ; i++){
-            cin >> a;
-            t[a]++;
-        }
-        for(int i = 1 ; i <= m ; i++){
-            if(t[i] >1){
-                count++;
-            }
-        }
-        cout <<  count << endl;
+	
+    int n ,k;
+    while(cin >> n >> k && n !=0 && k != 0){
+    	int ticket[100000] = {0};
+	    for(int i = 0; i < k ; ++i){
+			int a; cin >> a;
+			ticket[a]++;
+	    }
+	    int count = 0;
+	    for(int i = 1 ;i <= n;++i)
+	    {
+	    	if(ticket[i] > 1) count++;
+	    }
+	    cout << count << endl;
+		
     }
 
-
-    return 0;
+	return 0;
 }
