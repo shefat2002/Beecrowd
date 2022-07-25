b40f11d9ac9ed587fc1bd321afd111eca29fade3
Shefat Al Mahmud <shefat2002@gmail.com>
2022-07-25 21:05:39 +0600

    Create  1428 - Searching for Nessy.cpp

---
+++ 1428 - Searching for Nessy.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+     int a , b;
+     cin >> a >> b;
+     cout << (a/3)* (b/3);
+
+}
+
+int main()
+{
+    int n; cin >> n;
+    while(n--){
+        solve();
+        cout << endl;
+    }
+    return 0;
+}
